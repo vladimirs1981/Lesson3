4c86078b1f17db41d2aec21111d3ca9a4a1a2826
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-26 19:27:00 +0200

    AreYouHappy

--- src/exercises/AreYouHappy.java
+++ src/exercises/AreYouHappy.java
@@ -1,7 +1,28 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 /* See the diagram in the "doc" directory. */
 
 public class AreYouHappy {
 
+	public static void main(String[] args) {
+
+		String answer = JOptionPane.showInputDialog(null, "Are you happy?");
+
+		if (answer.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+		}
+		else if (answer.equals("no")) {
+			String answer1 = JOptionPane.showInputDialog(null, "Do you want to be happy?");
+			if (answer1.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "Change something");
+			} else if (answer1.equals("no")) {
+				JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong input");
+		}
+
+	}
 }
