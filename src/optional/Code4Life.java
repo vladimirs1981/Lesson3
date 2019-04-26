fa5c22d626934cdccdc0f3bce434c73423f3591b
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-26 19:34:26 +0200

    Code4Life

--- src/optional/Code4Life.java
+++ src/optional/Code4Life.java
@@ -5,18 +5,25 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
-	/*
-	 * Ask the user how many hours they spent coding this week.
-	 * 
-	 * 1. If it's 3 or more, tell them they're a Code Ninja.
-	 * 
-	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
-	 * 
-	 * 3. If it's more than 5, play the Batman theme song.
-	 */
-	
+	public static void main(String[] args) {
+
+		String hours = JOptionPane.showInputDialog(null, "How many hours you spent coding this week?");
+		int hoursAsInt = Integer.parseInt(hours);
+
+		if (hoursAsInt >= 3 && hoursAsInt < 5) {
+			JOptionPane.showMessageDialog(null, "You're a Code Ninja!");
+		} else if (hoursAsInt < 2) {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead!");
+		} else if (hoursAsInt > 5) {
+			playBatmanTheme();
+		} else {
+			JOptionPane.showMessageDialog(null, "Try harder!");
+		}
+
+	}
 
 	private static void playBatmanTheme() {
 		try {
@@ -31,4 +38,3 @@ public class Code4Life {
 	}
 
 }
-
