dfbca1721a578df80a8d9de1ce424d77ee19c892
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-26 19:38:01 +0200

    Validation

--- src/optional/Validation.java
+++ src/optional/Validation.java
@@ -2,20 +2,24 @@ package optional;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Validation {
 	public static void main(String[] args) {
 
-		// 1. Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		int randomNumber = new Random().nextInt(5);
-		// 2. On paper, write all the numbers that get printed when you run this class
-		System.out.println(randomNumber);
-
-		// 3. Use the randomNumber to give the user a random compliment.
-
-		// 4. Repeat all the code above 10 times
-		
-		// 5. Find someone to test out your program. They will like it :)
+		for (int i = 0; i < 10; i++) {
+			int randomNumber = new Random().nextInt(5);
+			if (randomNumber == 0) {
+				JOptionPane.showMessageDialog(null, "You're amazing person!");
+			} else if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "You look great, do you workout?");
+			} else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "You're beautiful!");
+			} else if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "You're smart and confident person!");
+			} else {
+				JOptionPane.showMessageDialog(null, "You got great personality");
+			}
+		}
 	}
 }
-
-
