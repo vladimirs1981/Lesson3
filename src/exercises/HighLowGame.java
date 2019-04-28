8a02c414fb4f843636b2db0e5919764888068232
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-04-28 10:24:08 +0200

    Change to JOptionPane

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -3,6 +3,8 @@ package exercises;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
@@ -10,22 +12,22 @@ public class HighLowGame {
 		Scanner sc = new Scanner(System.in);
 		int tries = 5;
 		int answer = rand.nextInt(100);
+		JOptionPane.showMessageDialog(null, "Good luck picking a number");
 		for (int i = 0; i < 5; i++) {
-			System.out.println("Guess a number: ");
-			int number = sc.nextInt();
+			String number = JOptionPane.showInputDialog(null, "Guess a number");
+			int numberAsInt = Integer.parseInt(number);
 			tries--;
 			if (tries == 0) {
-				System.out.println("You lose");
+				JOptionPane.showMessageDialog(null, "You Lose!");
 				break;
 			}
-			if (number == answer) {
-				System.out.println("Correct, you pick a right number!");
+			if (numberAsInt == answer) {
+				JOptionPane.showMessageDialog(null, "Correct, you pick a right number!");
 				break;
-			} else if (number > answer) {
-				System.out.println("To high, " + tries + " more tries");
+			} else if (numberAsInt > answer) {
+				JOptionPane.showMessageDialog(null, "To high, " + tries + " more tries");
 			} else {
-				System.out.println("To low, " + tries + " more tries");
-
+				JOptionPane.showMessageDialog(null, "To low, " + tries + " more tries");
 			}
 
 		}
