b4db2f8b60d0d88dd4f22c8c2dd19b8700a57f0e
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-03 15:28:48 +0200

    CountQuarters

--- src/optional/CountQuarters.java
+++ src/optional/CountQuarters.java
@@ -1,20 +1,24 @@
+
 package optional;
 
 import junit.framework.TestCase;
 
-/* Write a method called countQuarters(int money) that calculates the number of cents left over 
+/* Complete the countQuarters() method so that it calculates the number of cents left over 
  * if you took out all the possible quarters. */
 
 public class CountQuarters extends TestCase {
 	
 	public void test() throws Exception {
-		assertEquals(2, countQuarters(64));
-		assertEquals(3, countQuarters(1278));
+		assertEquals(0, countQuarters(0.25));
+		assertEquals(0, countQuarters(1));
+		assertEquals(14, countQuarters(0.64));
+		assertEquals(3, countQuarters(12.78));
 	}
 
-	private Object countQuarters(int i) {
-		// TODO Auto-generated method stub
-		return null;
+	private Object countQuarters(double money) {
+		
+		
+		return (int)(money*100)%25;
 	}
 
 }
